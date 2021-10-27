b766bc5aba04dd63f176d2d2779d99fc9c910859
Uchenna Chima <ifychim@gmail.com>
2021-10-27 00:32:49 -0400

    Refactored Navbar and home page out of App in client.  Started creating Auth component. Implemented usage of react router dom to handle routing within front-end.

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,31 @@
-import React, {useState,useEffect} from "react";
-//Components used in this app
-import {Container, Grow, Grid} from "@material-ui/core";
+import React from "react";
+import {Container} from "@material-ui/core";
+//Make our app multi-page using react-router-dom
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
-//Importing Logo
-//import furnitureExchange from "./images/furnitureExchange.png";
-
-//Importing user-created Components
-import Form from "./components/Form/Form";
-import Posts from "./components/Posts/Posts";
-
-//importing styles with materialUI
-import useStyles from "./styles";
-
-//redux hooks allows us to dispatch an action
-import { useDispatch } from "react-redux";
-
-//actions
-import {getPosts} from './actions/posts'
+//User-Defined Components used in this app
 import Navbar from "./components/Navbar/Navbar";
-const App = () => {
-    const classes = useStyles();
-    const dispatch = useDispatch();
-    const [currentId, setCurrentId] = useState(null);
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
 
-    useEffect(()=> {
-        dispatch(getPosts());
-    }, [currentId, dispatch]);
-    
+
+const App = () => {
     return (
         //Container centers everything with max width of large.
-        <Container maxwidth="lg">
+        <BrowserRouter>
 
-            <Navbar/>
-            
-            <Grow in>
-                <Container>
-                    <Grid className={classes.mainContainer} container  justifyContent="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}> 
-                            <Posts setCurrentId={setCurrentId}/>
-                        </Grid>
+            <Container maxwidth="lg">
+                <Navbar/>
+                
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/auth" exact component={Auth}/>
+                </Switch>
 
-                        <Grid item xs={12} sm={4}> 
-                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
+            </Container>
 
-        </Container>
+        </BrowserRouter>
+
     );
 }
 
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { AppBar, Typography } from '@material-ui/core';
+import { AppBar, Avatar, Button, Typography, Toolbar } from '@material-ui/core';
 import useStyles from './styles';
 //Importing Logo
 import furnitureExchange from '../../images/furnitureExchange.png';
+//Make our app multi-page using react-router-dom
 import { Link } from 'react-router-dom';
-//component={Link} to="/" -> is where app is crashing right now, remove it to view UI.
+
 const Navbar = () => {
     const classes = useStyles();
+    //Mock user, can be later filled.
+    const user = null;
     return (
         <AppBar className={classes.appBar} position = "static" color="inherit">
             <div className={classes.brandContainer}> 
@@ -16,8 +19,24 @@ const Navbar = () => {
                 </Typography>
                 <img className ={classes.img} src={furnitureExchange} alt="furnitureExchange" height="60"/>
             </div>
+            <Toolbar className={classes.toolbar}>
+                {user ? (
+                    <div className={classes.profile}> 
+                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)} </Avatar>
+                        <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+                        <Button variant="contained" className={classes.logout} color="secondary">
+                            Logout
+                        </Button>
+                    </div>
+                ): (
+                    <Button component={Link} to="/auth" color="primary" variant="contained"> 
+                        Sign In
+                    </Button>
+                )}
+            </Toolbar>
         </AppBar>
     );
 };
 
+//component={Link} to="/" -> is where app is crashing right now, remove it to view UI. Stopped at 15minutes.
 export default Navbar;
