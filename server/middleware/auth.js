6a953719c57a82d3a862b8d83db4a8586a2e2605
Uchenna Chima <ifychim@gmail.com>
2021-11-04 16:21:37 -0400

    finished implementing auth middle ware in backend.
    //wants to like a post steps
    //clicks like button => auth middleware (to confirm/deny the request to like a post) if middle ware returns next() then we call like controller. We use middle ware in routes.

---
+++ server/middleware/auth.js
@@ -0,0 +1,35 @@
+import jwt, { decode } from 'jsonwebtoken';
+
+//wants to like a post steps
+//clicks like button => auth middleware (to confirm/deny the request to like a post) if middle ware returns next() then we call like controller. We use middle ware in routes.
+
+const auth = async (req,res,next) => {
+
+    try {
+        //see if the user is really who he claims to be. we do this by utilizing the token
+        const token = req.headers.authorizaton.split(" ")[1];
+        const isCustomAuth = token.length < 500;
+
+        //data we want to get from the token
+        let decodedData;
+
+        if(token && isCustomAuth){
+            //gives data from each specific token, user name and id. secret has to be same as initial creation.
+            decodedData = jwt.verify(token, 'test');
+
+            req.userId = decodedData?.id;
+
+        }else {
+            //getting google auth
+            decodedData = jwt.decode(token);
+
+            req.userId = decodedData?.sub;
+        }
+
+        next();
+    } catch (error) {
+        
+    };
+};
+
+export default auth;
